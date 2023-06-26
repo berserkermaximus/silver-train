36fb58a258433b388bdf0c698665391a34ed341b
berserkermaximus <ansknim977@gmail.com>
2023-06-27 01:02:14 +0530

    created a database and Contact table

--- app/model.py
+++ app/model.py
@@ -1,11 +1,13 @@
 from .database import Base
-from sqlalchemy import Column, String, Integer, Enum, DateTime
+from sqlalchemy import Column, String, Integer, Enum, DateTime, Constraint, ForeignKeyConstraint, Index
 from sqlalchemy.sql.functions import now
 from .schema import Precedence
+from .database import engine
 
 
 class Contact(Base):
     __tablename__ = "contact"
+    # adding columns
     id = Column(Integer, autoincrement="auto",
                 nullable=False, primary_key=True)
     email = Column(String, nullable=True)
@@ -16,5 +18,23 @@ class Contact(Base):
     updatedAt = Column(DateTime, nullable=False, default=now())
     deletedAt = Column(DateTime, nullable=True)
 
+    # adding constraints
+
+    # both email and phoneNumber cannot be null
+    Constraint("contact_phoneNumber_email_not_null",
+               comment="phoneNumber is not NULL or email is not NULL")
+
+    # linkedId is a foreignkey from id
+    ForeignKeyConstraint(name="contact_linkedid_fkey",
+                         columns=[linkedId], refcolumns=[id])
+
+    # creating unique index on phoneNumber and email
+    Index("contact_phoneNumber_email_idx", phoneNumber, email, unique=True)
+
     class Config:
         orm_mode = True
+
+
+if __name__ == '__main__':
+    Contact.__table__.drop(engine)
+    Base.metadata.create_all(engine)
